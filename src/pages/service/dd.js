ceb7bc51953faf83878b960e9ee922782477472c
Nishant <nknishant91@gmail.com>
2023-04-14 09:37:16 +0530

    today get static props and theory

--- src/pages/service/dd.js
+++ src/pages/service/dd.js
@@ -1,14 +1,39 @@
 import Header from "@/components/layout/Header"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
-export default ()=>{
+export default (props)=>{
+    const [data,setData]  = useState([])
+    useEffect(()=>{
+            Promise.resolve(fetch(`http://localhost:3002/crud/listEmployee`)).then(ele=>{
+                setData(ele)
+            })
+    },[])
     return(
         <>
         <Header />
-        <p>Hello Service DD</p>
+        <p>Hello Service DD {props.name}</p>
+        {
+            props.apiData.map(ele=>
+                
+                <h1>{ele.empName}</h1>
+                )
+        }
 
             <Link href="/abc/[]/3" as="/abc/3">Go to abc</Link>
+
         </>
       
     )
+}
+
+export async function getStaticProps(){
+      const res = await  fetch(`http://localhost:3002/crud/listEmployee`)
+    
+    return {
+        props:{
+            name:"mukesh",
+            apiData:await res.json()
+        }
+    }
 }
