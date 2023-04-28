640707f5e066e41115655333f84a2f1d44ea21d4
Nishant <nknishant91@gmail.com>
2023-04-28 09:30:57 +0530

    use swr hooks

---
+++ src/pages/swr/index.js
@@ -0,0 +1,15 @@
+import useSWR from 'swr'
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+export default ()=>{
+    const { data, error } = useSWR(`http://localhost:3002/crud/listEmployee`,fetcher)
+    return(
+        <>
+        <h1>Hello</h1>
+        {
+              data && data.map((ele,index)=>
+                <p key = {index}>{ele.empName}</p>
+                )
+        }
+        </>
+    )
+}
